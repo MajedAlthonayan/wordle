a1c7617ef249d4c83e497753804513721b6a2843
Majed Althonayan <97466204+MajedAlthonayan@users.noreply.github.com>
2022-06-03 20:11:29 +0100

    added keypad to page

--- f1-wordle/src/components/Wordle.js
+++ f1-wordle/src/components/Wordle.js
@@ -1,27 +1,29 @@
 import React, { useEffect } from 'react'
-import { directive } from '@babel/types';
-import useWordle from '../hooks/useWordle';
-import Grid from './Grid';
+import useWordle from '../hooks/useWordle'
+
+// components
+import Grid from './Grid'
+import Keypad from './Keypad'
 
 export default function Wordle({ solution }) {
+  const { currentGuess, guesses, turn, isCorrect, handleKeyup } = useWordle(solution)
+  
+  useEffect(() => {
+    window.addEventListener('keyup', handleKeyup)
 
-    const { currentGuess, handleKeyup, guesses, isCorrect, turn } = useWordle(solution)
+    return () => window.removeEventListener('keyup', handleKeyup)
+  }, [handleKeyup])
 
-    useEffect(() => {
-        window.addEventListener('keyup', handleKeyup)
+  useEffect(() => {
+    console.log(guesses, turn, isCorrect)
+  }, [guesses, turn, isCorrect])
 
-        return () => window.removeEventListener('keyup', handleKeyup)
-    }, [handleKeyup])
-
-    useEffect(() => {
-        console.log(guesses, turn, isCorrect)
-    }, [guesses, turn, isCorrect])
-
-    return (
-        <div>
-            <div>solution - {solution}</div>
-            <div>current guess - {currentGuess}</div>
-            <Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
-        </div>
-    )
+  return (
+    <div>
+      <div>solution - {solution}</div>
+      <div>Current Guess - {currentGuess}</div>
+      <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
+      <Keypad />
+    </div>
+  )
 }
