14ba3cb468c97437f0089fff73faee892fda8c67
Majed Althonayan <97466204+MajedAlthonayan@users.noreply.github.com>
2022-06-03 20:26:38 +0100

    added colours to the keypad

--- f1-wordle/src/hooks/useWordle.js
+++ f1-wordle/src/hooks/useWordle.js
@@ -5,6 +5,7 @@ const useWordle = (solution) =>{
     const [guesses, setGuesses] = useState([...Array(6)])
     const [history, setHistory] = useState([])
     const [isCorrect, setIsCorrect] = useState(false)
+    const [usedKeys, setUsedKeys] = useState({})
 
 
     const formatGuess = () => {
@@ -49,6 +50,29 @@ const useWordle = (solution) =>{
             return prevTurn + 1
         })
 
+        setUsedKeys((prevUsedKeys) => {
+            let newKeys = {...prevUsedKeys}
+
+            formattedGuess.forEach((l) => {
+                const currentColor = newKeys[l.key]
+
+                if(l.color === 'green'){
+                    newKeys[l.key] = 'green'
+                    return
+                } 
+                if (l.color === 'yellow' && currentColor !== 'green') {
+                    newKeys[l.key] = 'yellow'
+                    return
+                }
+                if(l.color === 'grey' && currentColor !== 'green' && currentColor !== 'yellow'){
+                    newKeys[l.key] = 'grey'
+                    return
+                }
+
+            })
+            return newKeys
+        })
+
         setCurrentGuess('')
 
     }
@@ -84,7 +108,7 @@ const useWordle = (solution) =>{
         }
     }
 
-    return {turn, currentGuess, guesses, isCorrect, handleKeyup}
+    return {turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup}
 
 }
 export default useWordle
